700b4e6117b353a476950e2ac01b6eb2885945e8
U2625 <nastassii@gmail.com>
2019-04-12 19:24:01 +0300

    Pervye 2 zadachi

---
+++ hexdec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+    int x = 0xFF;
+    int y = 077;
+    printf("hex x = %x\n", x);
+    printf("dec x = %d\n", x);
+    printf("oct x = %o\n", x);
+    printf("oct y = %o\n", y);
+    printf("dec y = %d\n", y);
+    printf("hex y = %x\n", y);
+    return 0;
+}
