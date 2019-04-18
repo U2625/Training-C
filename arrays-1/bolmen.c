8a2720c94ccb60cb3692fb04333bb8aa81d5d0e1
U2625 <nastassii@gmail.com>
2019-04-18 18:58:52 +0300

    вывод на экран всех элементов массива, которые меньше последнего элемента и больше первого элемента массива

---
+++ arrays-1/bolmen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n=0;
+  int i=0;
+  int k=0;
+
+  scanf("%d", &n);
+  int arr[n];
+
+  for(i = 0; i < n ; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+
+  for(i = 0; i < n; i++)
+  {
+          if (arr[i] < arr[n-1] && arr[i] > arr[0])
+          {
+                  printf("%d ", arr[i]);
+                  k++;
+          }
+  }
+  if (k==0) printf("0\n");
+  return 0;
+}
