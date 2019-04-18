9767c5d2ee4dc35de710a7667a7926adef02a9e2
U2625 <nastassii@gmail.com>
2019-04-18 18:33:14 +0300

    Сначала чётные элементы массива в том порядке, как они встречаются в массиве, затем нечётные элементы массива в том порядке, как они встречаются в массиве

---
+++ arrays-1/chetnechet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n=0;
+  int i=0;
+
+  scanf("%d", &n);
+  int arr[n];
+
+  for(i = 0; i < n ; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+
+  for(i = 0; i < n; i++)
+  {
+          if (arr[i]%2 == 0)
+                  printf("%d ", arr[i]);
+  }
+
+  for(i = 0; i < n; i++)
+  {
+          if (arr[i]%2 != 0)
+                  printf("%d ", arr[i]);
+  }
+
+  return 0;
+}
+
