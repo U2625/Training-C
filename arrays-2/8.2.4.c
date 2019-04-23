5eeecef116c3922223d4b4209517e4f927517317
U2625 <nastassii@gmail.com>
2019-04-23 20:44:15 +0300

    По заданному числу N сформировать матрицу (𝑁×𝑁) следующего вида:1    2    3   ...  n-2  n-1  n  .......

---
+++ arrays-2/8.2.4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int n,m; scanf("%d %d",&n,&m);
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+      for(int j = 0; j < n; j++)
+      {
+        if(j <= i)
+           arr[i][j] = 1+i-j;
+        else
+           arr[i][j] = 1+fabs(i-j);
+      }
+    }
+    for (int i = 0; i < n; i++)
+    {
+       for(int j = 0; j < n; j++)
+       {
+            printf("%d ",arr[i][j]);
+       }
+        printf("\n");
+    }
+    return 0;
+}
