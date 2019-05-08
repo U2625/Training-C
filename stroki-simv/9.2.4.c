39d79f30f5110f8d5a7c33429c6f415ddedbf018
U2625 <nastassii@gmail.com>
2019-05-08 20:11:41 +0300

    Дана строка изображающая двоичную запись целого положительного числа. Вывести строку, изображающую десятичную запись этого же числа

---
+++ stroki-simv/9.2.4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n; int sum = 0; int i = 0; int a = 0;
+    char ch[100];
+
+    fgets(ch, 100, stdin);
+
+    while (ch[i] != '\0') {i++;}        //vsego simv. v stroke
+
+    for (int j=0; j<i; j++)
+    {
+      if (ch[j] == '1')
+            sum += pow(2, (i-j-1));
+    }
+
+    printf("\n%d\n", sum);
+
+    return 0;
+}
