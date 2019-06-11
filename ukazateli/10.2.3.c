abfd6e2cdadd7c0f96d6cd4d700d24f58e6f9d86
U2625 <nastassii@gmail.com>
2019-06-11 17:39:14 +0300

    vyvesti znachenija, na kotorye ssylayutsya ukazateli...

---
+++ ukazateli/10.2.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int a = 0, b = 0;
+ 
+  scanf("%d %d", &a, &b);
+
+  int * p_1 = &b;
+  int * p_2 = &a;
+
+  printf("%d %d",*p_2, *p_1);
+
+  return 0;
+}
