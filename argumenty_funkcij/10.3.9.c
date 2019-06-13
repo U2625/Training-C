a84dd2c03ea48b7e1459127916d6dc7a775b4c6e
U2625 <nastassii@gmail.com>
2019-06-13 13:39:06 +0300

    Напишите функцию int minmax(int arr[], int n, int fl), которая возвращает максимальный или минимальный элемент массива arr, в зависимости от значения флага fl. 0 -- минимум, 1 -- максимум

---
+++ argumenty_funkcij/10.3.9.c
@@ -0,0 +1,20 @@
+int minmax(int arr[], int n, int fl)
+{ 
+  int min = 9999;
+  int max = -9999;
+    
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] < min)
+        min = arr[i];
+    if (arr[i] > max)
+        max = arr[i];
+  }
+  switch (fl)
+  {
+      case 0:
+          return min; break;
+      case 1:
+          return max; break;
+  }
+}
